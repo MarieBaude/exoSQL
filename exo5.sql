182a0684987c9a969bb93ad4bd994d9f4dd49100
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-02 16:37:49 +0100

    add

--- exo5.sql
+++ exo5.sql
@@ -1,16 +1,20 @@
--- Lister tous les types de postes n’existant pas dans le parc informatique
-SELECT * 
+-- Lister tous les types de postes n’existant pas dans le parc informatique /chercher NC
+SELECT TYPE_POSTE 
 FROM POSTE
-WHERE TYPE_POSTE 
+INNER JOIN TYPE ON  ;
 
 -- Lister les types existants à la fois comme poste et comme logiciel
-
+SELECT TYPE_LP 
+FROM Type
+INNER JOIN LOGICIEL ON TYPE_POSTE = ;
 
 -- Lister les types qui existent en tant que poste mais pas en tant que logiciel
 
 
 -- Lister les ip des postes qui contiennent le logiciel « Log6 »
-
+SELECT N_SEGMENT
+FROM POSTE
+INNER JOIN LOGICIEL ON N_LOGICIEL = 'Log6';
 
 -- Lister les ip des postes qui contiennent le logiciel de nom « Oracle 9i »
 SELECT N_SEGMENT,  NOM_LOGICIEL
@@ -19,6 +23,11 @@ INNER JOIN LOGICIEL ON NOM_LOGICIEL = 'Oracle 9i';
 
 
 -- Lister le nom des segments possédant exactement trois postes de type TX
-
+SELECT NOM_SEGMENT, TYPE_POSTE
+FROM SEGMENT
+INNER JOIN POSTE ON TYPE_POSTE = 'TX';
 
 -- Lister le nom des salles où l’on peut trouver au moins un poste avec le logiciel « Oracle 10g » installé
+SELECT NOM_SALLE
+FROM SALLE
+INNER JOIN LOGICIEL ON NOM_LOGICIEL = 'Oracle 10g';
--- exo6.sql
+++ exo6.sql
@@ -1,4 +1,6 @@
 -- Compter le nombre de postes de chaque type
-
+SELECT COUNT(*) 
+FROM POSTE 
+;
 
 -- Compter le nombre de logiciels installés par année
