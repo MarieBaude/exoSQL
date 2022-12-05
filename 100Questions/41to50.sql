8808a04923875fe65b9db481b1c8f6489d5ade1d
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-05 16:37:48 +0100

    add 42 44

--- 100Questions/41to50.sql
+++ 100Questions/41to50.sql
@@ -5,7 +5,10 @@ WHERE noserv = 3
 ORDER BY sal DESC;
 
 /*42 : Idem en indiquant le numéro de colonne à la place du nom colonne.*/
-
+SELECT nom AS "1", prenom AS "2", noserv AS "3", sal AS "4"
+FROM emp
+WHERE noserv = 3
+ORDER BY sal DESC;
 
 /*43 : Trier les employés (nom, prénom, n° de service, salaire, emploi) par emploi, et pour chaque emploi par ordre décroissant de salaire.*/
 SELECT nom, prenom, noserv, sal, emploi
@@ -13,7 +16,9 @@ FROM emp
 ORDER BY emploi DESC, sal DESC;
 
 /*44 : Idem en indiquant les numéros de colonnes.*/
-
+SELECT nom AS "1", prenom AS "2", noserv AS "3", sal AS "4", emploi AS "5"
+FROM emp
+ORDER BY emploi DESC, sal DESC;
 
 /*45 : Trier les employés (nom, prénom, n° de service, commission) du service3 par ordre croissant de commission.*/
 SELECT nom, prenom, noserv, comm
@@ -29,10 +34,14 @@ AND comm IS NOT NULL
 ORDER BY comm DESC;
 
 /*47 : Sélectionner le nom, le prénom, l'emploi, le nom du service de l'employé pour tous les employés.*/
-
+SELECT nom, prenom, emploi, service
+FROM emp
+INNER JOIN serv ON emp.noserv = serv.noserv;
 
 /*48 : Sélectionner le nom, l'emploi, le numéro de service, le nom du service pour tous les employés.*/
-
+SELECT emp.noserv, nom, prenom, emploi, service
+FROM emp
+INNER JOIN serv ON emp.noserv = serv.noserv;
 
 /*49 : Idem en utilisant des alias pour les noms de tables.*
 
