8079c5b1c4dd22904be807c3165fa9bef8f68218
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-05 11:01:35 +0100

    add 100 questions folder and 10 first questions

---
+++ 100Questions/1to10.sql
@@ -0,0 +1,12 @@
+/*1:Sélectionner toutes les colonnes de la table SERV.*/
+
+
+/*2:Sélectionner d’une autre manière ces colonnes.*/
+/*3:Sélectionner les colonnes SERVICE et NOSERV de la table SERV.*/
+/*4:Sélectionner toutes les colonnes de la table EMP.*/
+/*5:Sélectionner les emplois de la table EMP.*/
+/*6:Sélectionner les différents emplois de la table EMP.*/
+/*7 : Sélectionner les employés du service N°3.*/
+/*8 : Sélectionner les noms, prénoms, numéro d’employé, numéro de service de tous les techniciens.*/
+/*9 : Sélectionner les noms, numéros de service de tous les servicesdont le numéro est supérieur à 2.*/
+/*10 : Sélectionner les noms, numéros de service de tous les services dont le numéro est inférieur ou égal à 2.*/
