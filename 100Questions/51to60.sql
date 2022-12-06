03fc8596431f05bd5ed2c5676056aa2dc8d743f4
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-06 15:51:44 +0100

    adding file 51 to 60

---
+++ 100Questions/51to60.sql
@@ -0,0 +1,28 @@
+/*51 : Sélectionner les nom et date d'embauche des employés suivi des nom et date d'embauche de leur supérieur pour les employés plus ancien que leur supérieur, dans l'ordre nom employés, noms supérieurs.*/
+
+
+/*52 : Sélectionner sans doublon les prénoms des directeurs et « les prénoms des techniciens du service 1 » avec un UNION.*/
+
+
+/*53 : Sélectionner les numéros de services n’ayant pas d’employés sans une jointure externe.*/
+
+
+/*54 : Sélectionner les services ayant au moins un employé.*/
+
+
+/*54 bis : Sélectionner les numéros de services ayant des employés sans une jointure externe*/
+
+
+/*56 : Sélectionner les employés qui ont le même chef que DUBOIS, DUBOIS exclu.*/
+
+
+/*57 : Sélectionner les employés embauchés le même jour que DUMONT.*
+
+
+/*58 : Sélectionner les nom et date d'embauche des employés plus anciens que MINET, dans l’ordre des embauches.*/
+
+
+/*59 : Sélectionner le nom, le prénom, la date d’embauche des employés plus anciens que tous les employés du service N°6. (Attention MIN)*/
+
+
+/*60 : Sélectionner le nom, le prénom, le revenu mensuel des employés qui gagnent plus qu'au moins un employé du service N°3, trier le résultat dans l'ordre croissant des revenus mensuels.*/
