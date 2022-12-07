9ce9127c8c3b597c3acb08516c0a14fce99def8e
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-07 09:33:02 +0100

    add 51, 52

--- 100Questions/51to60.sql
+++ 100Questions/51to60.sql
@@ -1,11 +1,18 @@
 /*51 : Sélectionner les nom et date d'embauche des employés suivi des nom et date d'embauche de leur supérieur pour les employés plus ancien que leur supérieur, dans l'ordre nom employés, noms supérieurs.*/
-SELECT nom, embauche, emploi
-FROM emp
-WHERE emploi != 'DIRECTEUR' OR emploi != 'PRESIDENT'
-
-;
+SELECT e.nom, e.embauche, e.emploi, s.nom, s.embauche, s.emploi
+FROM emp e, emp s
+WHERE e.embauche < s.embauche
+AND e.sup = s.noemp
+ORDER BY e.nom, s.nom;
 
 /*52 : Sélectionner sans doublon les prénoms des directeurs et « les prénoms des techniciens du service 1 » avec un UNION.*/
+SELECT DISTINCT prenom
+FROM emp 
+WHERE emploi = 'DIRECTEUR'
+UNION 
+SELECT DISTINCT prenom
+FROM emp
+WHERE emploi = 'TECHNICIEN' AND noserv = 1;
 
 
 /*53 : Sélectionner les numéros de services n’ayant pas d’employés sans une jointure externe.*/
