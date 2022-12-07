b6f9ed0a4b19a9c2e126827527134d43dba8681e
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-07 15:33:19 +0100

    add

--- 100Questions/61to70.sql
+++ 100Questions/61to70.sql
@@ -5,21 +5,27 @@ INNER JOIN serv ON emp.noserv = serv.noserv
 WHERE serv.ville = 'LILLE';
 
 /*62 : Sélectionner les employés du service 1, ayant le même emploi qu'un employé du service N°3.*/
--- SELECT nom, prenom, noserv, emploi
--- FROM emp emp1
--- WHERE noserv = 1
---     (SELECT nom, prenom, noserv, emploi
---     FROM emp emp2
---     WHERE noserv = 3
---     AND emp1.emploi = emp2.emploi);
-
+SELECT nom, prenom, noserv, emploi
+FROM emp emp1
+WHERE noserv = 1
+    -- (SELECT emploi
+    -- FROM emp emp2
+    -- WHERE noserv = 3
+    -- AND emp1.emploi = emp2.emploi)
+;
 
 
 /*63 : Sélectionner les employés du service 1 dont l'emploi n'existe pas dans le service 3.*/
 
 
 /*64 : Sélectionner nom, prénom, emploi, salaire pour les employés ayant même emploi et un salaire supérieur à celui de CARON.*/
-
+SELECT nom, prenom, emploi, sal
+FROM emp emp1
+WHERE sal >
+    (SELECT 
+    FROM emp emp2
+    WHERE emp1.noserv = emp2.noserv)
+;
 
 /*65 : Sélectionner les employés du service N°1 ayant le même emploi qu'un employé du service des VENTES.*/
 
@@ -38,7 +44,7 @@ WHERE sal >
 ORDER BY noserv;
 
 /*68 : Sélectionner les employés du service INFORMATIQUE embauchés la même année qu'un employé du service VENTES.( année : to_char(embauche,’YYYY’) )*/
-
+-- inner join et where et in
 
 /*69 : Sélectionner le nom, l’emploi, la ville pour les employés qui ne travaillent pas dans le même service que leur supérieur hiérarchique direct.*/
 
